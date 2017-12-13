442293f140987e43f7df4e84f0bb92219b271b65
Stephan Lonntorp <stephan.kvart@creuna.se>
2017-12-13 11:28:36 +0100

    Modified nuspec and assemblyinfo to remove Creuna trademark

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using EPiServer.PlugIn;
 
 [assembly: AssemblyTitle("URL Transliteration")]
 [assembly: AssemblyDescription("URL Transliteration for EPiServer CMS")]
-[assembly: AssemblyCompany("Creuna AB")]
-[assembly: AssemblyCopyright("Copyright 2017 Creuna AB")]
+[assembly: AssemblyCompany("@defsteph")]
+[assembly: AssemblyCopyright("Copyright 2017")]
 [assembly: AssemblyProduct("EPi.UrlTransliterator")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4206e11f-3084-48b7-9ec3-203d2e5edbb2")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: PlugInSummary(MoreInfoUrl = "<A href='https://github.com/CreunaAB/EPi.UrlTransliterator'>GitHub Repository</A>", License = LicensingMode.Freeware)]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: PlugInSummary(MoreInfoUrl = "<A href='https://github.com/defsteph/EPi.UrlTransliterator'>GitHub Repository</A>", License = LicensingMode.Freeware)]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
